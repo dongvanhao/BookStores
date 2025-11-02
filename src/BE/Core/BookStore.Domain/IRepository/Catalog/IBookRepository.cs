5b2596e9e70728d09d9ebade57febc3ff2ecde14
DongHao <dongvanhao12345@gmail.com>
2025-11-02 22:50:37 +0700

    feat<Domain>: add UnitOfWork

---
+++ src/BE/Core/BookStore.Domain/IRepository/Catalog/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Domain.Entities.Catalog;
+using BookStore.Domain.IRepository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Domain.IRepository.Catalog
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+        Task<Book?> GetBookWithDetailsAsync(Guid id);
+    }
+}
